d06e1c490d5e52863b67790d46b25be6fab92a8a
vijay <vijay@vijay-A14RM0E.(none)>
2016-05-30 07:59:09 +0530

    initial commit

---
+++ Desktop/README.md/Movies/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '73eea299047bf11edab5362a607576552560f42373507d99af05b7d928ceb56d478573669618d02c69413512e227cedbc5ba1dc2aff57556930063b75be2a651';
